4a066927c4deeff1581c7a65e53bb7399ea48ca7
Jason Barrie Morley <jbmorley@mac.com>
2015-01-20 09:27:17 +0000

    Initial addition of the locations plugin file.

---
+++ locations.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Locations
+ * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Description: A brief description of the plugin.
+ * Version: 1.0.0
+ * Author: Jason Barrie Morley
+ * Author URI: http://jbmorley.co.uk
+ * Text Domain: Optional. Plugin's text domain for localization. Example: mytextdomain
+ * License: A short license name. Example: GPL2
+ */
+?>
