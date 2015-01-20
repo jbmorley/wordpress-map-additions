576c133cdf5b4e8e6f5253a4f7dd664229ff33a1
Jason Barrie Morley <jbmorley@mac.com>
2015-01-20 10:07:40 +0000

    Adding the Google Maps script and legacy locations script.

--- locations.php
+++ locations.php
@@ -9,4 +9,23 @@
  * Text Domain: Optional. Plugin's text domain for localization. Example: mytextdomain
  * License: A short license name. Example: GPL2
  */
+
+class locations {
+
+	static function init() {
+		$api_key = 'AIzaSyBxqKnZdnWREeJePXTLJZmIXulbQwCx_hk';
+		$path = plugins_url('js/locations.js', __FILE__);
+		wp_enqueue_script('locations', $path);
+		wp_enqueue_script('google-maps', 'https://maps.googleapis.com/maps/api/js?key=' . $api_key);
+	}
+
+	static function filter_something($content) {
+		return $content;
+	}
+
+}
+
+add_action('init', array('locations', 'init'));
+add_filter('the_content', array('locations', 'filter_something'));
+
 ?>
