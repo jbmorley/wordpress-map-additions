9df0459d9bd9efcf35ea46ce1136e774005f98a5
Jason Barrie Morley <jbmorley@mac.com>
2015-01-26 11:32:24 +0000

    Adding the appropriate license to the header file.

--- wordpress-map-additions.php
+++ wordpress-map-additions.php
@@ -1,14 +1,28 @@
 <?php
-/**
- * Plugin Name: WordPress Map Additions
- * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
- * Description: A brief description of the plugin.
- * Version: 1.0.0
- * Author: Jason Barrie Morley
- * Author URI: http://jbmorley.co.uk
- * Text Domain: Optional. Plugin's text domain for localization. Example: mytextdomain
- * License: GPL2
- */
+//
+// Plugin Name: WordPress Map Additions
+// Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+// Description: Collection of mapping extensions for WordPress.
+// Version: 1.0.0
+// Author: Jason Barrie Morley
+// Author URI: http://jbmorley.co.uk
+// License: GPL2
+//
+// Copyright 2015 Jason Barrie Morley (jbmorley@mac.com)
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License, version 2, as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
 
 class wordpress_map_additions {
 
