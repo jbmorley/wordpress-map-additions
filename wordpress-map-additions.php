e79274966e67d84208aff6c8b7fed2d7c434baf0
Jason Barrie Morley <jason.morley@inseven.co.uk>
2015-01-26 11:39:32 +0100

    Naming fixes.

--- wordpress-map-additions.php
+++ wordpress-map-additions.php
@@ -1,240 +1,239 @@
 <?php
 /**
- * Plugin Name: Locations
+ * Plugin Name: WordPress Map Additions
  * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
  * Description: A brief description of the plugin.
  * Version: 1.0.0
  * Author: Jason Barrie Morley
  * Author URI: http://jbmorley.co.uk
  * Text Domain: Optional. Plugin's text domain for localization. Example: mytextdomain
- * License: A short license name. Example: GPL2
+ * License: GPL2
  */
 
-class locations {
+class wordpress_map_additions {
 
-	const SETTINGS_PAGE = "general";
-	const SETTINGS_GROUP = "locations";
-	const SETTING_GOOGLE_MAPS_API_KEY = "locations_google_maps_api_key";
+    const SETTINGS_PAGE = "general";
+    const SETTINGS_GROUP = "wordpress_map_additions";
+    const SETTING_GOOGLE_MAPS_API_KEY = "wordpress_map_additions_google_maps_api_key";
 
-	static $maps = array();
-	static $galleryId = 0;
+    static $maps = array();
+    static $galleryId = 0;
 
-	static function init() {
+    static function init() {
 
-		$api_key = get_option(self::SETTING_GOOGLE_MAPS_API_KEY);
-		$path = plugins_url('js/wordpress-map-additions.js', __FILE__);
-		wp_enqueue_script('google-maps', 'https://maps.googleapis.com/maps/api/js?key=' . $asitepi_key);
-		wp_enqueue_script('locations', $path);
+        $api_key = get_option(self::SETTING_GOOGLE_MAPS_API_KEY);
+        wp_enqueue_script('google-maps', 'https://maps.googleapis.com/maps/api/js?key=' . $asitepi_key);
+        wp_enqueue_script('wordpress-map-additions', plugins_url('js/wordpress-map-additions.js', __FILE__));
 
-	}
+    }
 
-	static function admin_init() {
+    static function admin_init() {
 
-		add_settings_field(
-			self::SETTING_GOOGLE_MAPS_API_KEY,
-			'Google Maps API Key',
-			array('locations', 'settings_google_maps_api_key_callback'),
-			self::SETTINGS_PAGE,
-			'default');
+        add_settings_field(
+            self::SETTING_GOOGLE_MAPS_API_KEY,
+            'Google Maps API Key',
+            array('wordpress_map_additions', 'settings_google_maps_api_key_callback'),
+            self::SETTINGS_PAGE,
+            'default');
 
-		register_setting(self::SETTINGS_GROUP, self::SETTING_GOOGLE_MAPS_API_KEY);
+        register_setting(self::SETTINGS_GROUP, self::SETTING_GOOGLE_MAPS_API_KEY);
 
-	}
+    }
 
-	static function settings_google_maps_api_key_callback() {
-		self::input(self::SETTING_GOOGLE_MAPS_API_KEY);
-	}
+    static function settings_google_maps_api_key_callback() {
+        self::input(self::SETTING_GOOGLE_MAPS_API_KEY);
+    }
 
-	static function input($setting, $default = None) {
-		settings_fields(self::SETTINGS_GROUP);
-		$value = get_option($setting, $default);
-		echo '<input type="text" id="' . $setting . '" name="' . $setting . '" value="' . $value . '" />';
-	}
+    static function input($setting, $default = None) {
+        settings_fields(self::SETTINGS_GROUP);
+        $value = get_option($setting, $default);
+        echo '<input type="text" id="' . $setting . '" name="' . $setting . '" value="' . $value . '" />';
+    }
 
-	static function filter_content($content) {
-		$pattern = get_shortcode_regex();
-		$result = preg_replace_callback( "/$pattern/s", array('locations', 'handle_gallery_shortcode'), $content);
-		return $result;
-	}
+    static function filter_content($content) {
+        $pattern = get_shortcode_regex();
+        $result = preg_replace_callback( "/$pattern/s", array('wordpress_map_additions', 'handle_gallery_shortcode'), $content);
+        return $result;
+    }
 
-	static function get_map($id, $options = None) {
+    static function get_map($id, $options = None) {
 
-		$pins = array();
-		if (array_key_exists($id, static::$maps)) {
-			$pins = static::$maps[$id];
-		}
+        $pins = array();
+        if (array_key_exists($id, static::$maps)) {
+            $pins = static::$maps[$id];
+        }
 
-		$details = array("pins" => $pins, "zoom" => $options["zoom"]);
+        $details = array("pins" => $pins, "zoom" => $options["zoom"]);
 
-		$result = "";
-		$result .= "<script>";
-		$result .= "locations['" . $id . "'] = " . json_encode($details) . ";";
-		$result .= "</script>";
-		$result .= "<div id='locations-map-canvas-" . $id . "' style='width: 100%; height: 400px'></div>";
+        $result = "";
+        $result .= "<script>";
+        $result .= "locations['" . $id . "'] = " . json_encode($details) . ";";
+        $result .= "</script>";
+        $result .= "<div id='locations-map-canvas-" . $id . "' style='width: 100%; height: 400px'></div>";
 
-		return $result;
+        return $result;
 
-	}
+    }
 
-	static function shortcode_map($atts) {
-		$a = shortcode_atts(
-			array(
-				"id" => "default",
-				"zoom" => 4
-				),
-			$atts);
+    static function shortcode_map($atts) {
+        $a = shortcode_atts(
+            array(
+                "id" => "default",
+                "zoom" => 4
+                ),
+            $atts);
 
-		$id = $a["id"];
-		return self::get_map($id, $a);
-	}
+        $id = $a["id"];
+        return self::get_map($id, $a);
+    }
 
-	static function copy_array_keys($keys, $source_array) {
-		$result = array();
-		foreach ($keys as &$key) {
-			$result[$key] = $source_array[$key];
-		}
-		unset($key);
-		return $result;
-	}
+    static function copy_array_keys($keys, $source_array) {
+        $result = array();
+        foreach ($keys as &$key) {
+            $result[$key] = $source_array[$key];
+        }
+        unset($key);
+        return $result;
+    }
 
-	static function add_pin($mapId, $pin) {
+    static function add_pin($mapId, $pin) {
 
-		if (!array_key_exists($mapId, static::$maps)) {
-			static::$maps[$mapId] = array();
-		}
+        if (!array_key_exists($mapId, static::$maps)) {
+            static::$maps[$mapId] = array();
+        }
 
-		array_push(static::$maps[$mapId], $pin);
+        array_push(static::$maps[$mapId], $pin);
 
-		$index = count(static::$maps[$mapId]) - 1;
+        $index = count(static::$maps[$mapId]) - 1;
 
-		return $index;
+        return $index;
 
-	}
+    }
 
-	static function shortcode_pin($atts) {
-		$a = shortcode_atts(
-			array(
-				"map" => "default",
-				"name" => "Dropped Pin",
-				"lat" => "0.000",
-				"lng" => "0.000"
-				),
-			$atts);
+    static function shortcode_pin($atts) {
+        $a = shortcode_atts(
+            array(
+                "map" => "default",
+                "name" => "Dropped Pin",
+                "lat" => "0.000",
+                "lng" => "0.000"
+                ),
+            $atts);
 
-		$mapId = $a["map"];
+        $mapId = $a["map"];
 
-		$sanitised_pin = self::copy_array_keys(["name", "lat", "lng"], $a);
-		$index = self::add_pin($mapId, $sanitised_pin);
+        $sanitised_pin = self::copy_array_keys(["name", "lat", "lng"], $a);
+        $index = self::add_pin($mapId, $sanitised_pin);
 
-		return '<a href="javascript:setLocation(\'' . $mapId . '\', ' . $index . ');">' . $sanitised_pin["name"] . '</a>';
-	}
+        return '<a href="javascript:setLocation(\'' . $mapId . '\', ' . $index . ');">' . $sanitised_pin["name"] . '</a>';
+    }
 
-	function convert_gps($exifCoord, $hemi) {
+    function convert_gps($exifCoord, $hemi) {
 
-		$degrees = count($exifCoord) > 0 ? self::gps_to_number($exifCoord[0]) : 0;
-		$minutes = count($exifCoord) > 1 ? self::gps_to_number($exifCoord[1]) : 0;
-		$seconds = count($exifCoord) > 2 ? self::gps_to_number($exifCoord[2]) : 0;
+        $degrees = count($exifCoord) > 0 ? self::gps_to_number($exifCoord[0]) : 0;
+        $minutes = count($exifCoord) > 1 ? self::gps_to_number($exifCoord[1]) : 0;
+        $seconds = count($exifCoord) > 2 ? self::gps_to_number($exifCoord[2]) : 0;
 
-		$flip = ($hemi == 'W' or $hemi == 'S') ? -1 : 1;
+        $flip = ($hemi == 'W' or $hemi == 'S') ? -1 : 1;
 
-		return $flip * ($degrees + $minutes / 60 + $seconds / 3600);
+        return $flip * ($degrees + $minutes / 60 + $seconds / 3600);
 
-	}
+    }
 
-	function gps_to_number($coordPart) {
+    function gps_to_number($coordPart) {
 
-		$parts = explode('/', $coordPart);
+        $parts = explode('/', $coordPart);
 
-		if (count($parts) <= 0)
-			return 0;
+        if (count($parts) <= 0)
+            return 0;
 
-		if (count($parts) == 1)
-			return $parts[0];
+        if (count($parts) == 1)
+            return $parts[0];
 
-		return floatval($parts[0]) / floatval($parts[1]);
+        return floatval($parts[0]) / floatval($parts[1]);
 
-	}
+    }
 
-	static function get_gps_coordinate($key, $exif) {
+    static function get_gps_coordinate($key, $exif) {
 
-		$result = "0";
+        $result = "0";
 
-		$value_key = $key;
-		$direction_key = $value_key . "Ref";
+        $value_key = $key;
+        $direction_key = $value_key . "Ref";
 
-		if (!array_key_exists($value_key, $exif) || !array_key_exists($direction_key, $exif)) {
-			return None;
-		}
+        if (!array_key_exists($value_key, $exif) || !array_key_exists($direction_key, $exif)) {
+            return None;
+        }
 
-		$value = $exif[$value_key];
-		$direction = $exif[$direction_key];
+        $value = $exif[$value_key];
+        $direction = $exif[$direction_key];
 
-		if ($value == None || $direction == None) {
-			return None;
-		}
+        if ($value == None || $direction == None) {
+            return None;
+        }
 
-		return self::convert_gps($value, $direction);
-	}
+        return self::convert_gps($value, $direction);
+    }
 
-	static function get_gps($path) {
+    static function get_gps($path) {
 
-		$exif = exif_read_data($path);
+        $exif = exif_read_data($path);
 
-		$latitude = self::get_gps_coordinate("GPSLatitude", $exif);
-		$longitude = self::get_gps_coordinate("GPSLongitude", $exif);
+        $latitude = self::get_gps_coordinate("GPSLatitude", $exif);
+        $longitude = self::get_gps_coordinate("GPSLongitude", $exif);
 
-		if ($longitude === None || $latitude === None) {
-			return None;
-		}
+        if ($longitude === None || $latitude === None) {
+            return None;
+        }
 
-		return array($latitude, $longitude);
-	}
+        return array($latitude, $longitude);
+    }
 
-	static function handle_gallery_shortcode($m) {
+    static function handle_gallery_shortcode($m) {
 
-		$tag = $m[2];
+        $tag = $m[2];
 
-		$result = "";
+        $result = "";
 
-		if (strcmp($tag, "gallery") !== 0) {
-			return $m[0];
-		}
+        if (strcmp($tag, "gallery") !== 0) {
+            return $m[0];
+        }
 
-		self::$galleryId++;
-		$mapId = "post-" . get_the_ID() . "-gallery-" . self::$galleryId;
+        self::$galleryId++;
+        $mapId = "post-" . get_the_ID() . "-gallery-" . self::$galleryId;
 
-		$attr = shortcode_parse_atts($m[3]);
-		$ids = explode(',', $attr["ids"]);
-		foreach ($ids as &$id) {
-			if (wp_attachment_is_image($id)) {
-				$path = get_attached_file($id);
-				$gps = self::get_gps($path);
-				if ($gps !== None) {
-					$result .= var_export($gps, true);
-					self::add_pin($mapId, array("name" => "Pin", "lat" => $gps[0], "lng" => $gps[1]));
-				}
-			}
-		}
-		unset($id);
+        $attr = shortcode_parse_atts($m[3]);
+        $ids = explode(',', $attr["ids"]);
+        foreach ($ids as &$id) {
+            if (wp_attachment_is_image($id)) {
+                $path = get_attached_file($id);
+                $gps = self::get_gps($path);
+                if ($gps !== None) {
+                    $result .= var_export($gps, true);
+                    self::add_pin($mapId, array("name" => "Pin", "lat" => $gps[0], "lng" => $gps[1]));
+                }
+            }
+        }
+        unset($id);
 
-		$gallery = self::get_map($mapId, array("zoom" => 12));
+        $gallery = self::get_map($mapId, array("zoom" => 12));
 
-		return $m[0] . $gallery;
+        return $m[0] . $gallery;
 
-	}
+    }
 
 }
 
 // Actions
-add_action('init', array('locations', 'init'));
-add_action('admin_init', array('locations', 'admin_init'));
+add_action('init', array('wordpress_map_additions', 'init'));
+add_action('admin_init', array('wordpress_map_additions', 'admin_init'));
 
 // Shortcodes.
-add_shortcode('map', array('locations', 'shortcode_map'));
-add_shortcode('pin', array('locations', 'shortcode_pin'));
+add_shortcode('map', array('wordpress_map_additions', 'shortcode_map'));
+add_shortcode('pin', array('wordpress_map_additions', 'shortcode_pin'));
 
 // Filters
-add_filter('the_content', array('locations', 'filter_content'));
+add_filter('the_content', array('wordpress_map_additions', 'filter_content'));
 
 ?>
 
