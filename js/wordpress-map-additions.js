70009b788641d172895789860e45d514350ab3fb
Jason Barrie Morley <jason.morley@inseven.co.uk>
2015-01-26 11:45:57 +0100

    Converting tabs to spaces.

--- js/wordpress-map-additions.js
+++ js/wordpress-map-additions.js
@@ -1,59 +1,59 @@
 function locations_initialize() {
 
-	for (var key in locations) {
-		if (locations.hasOwnProperty(key)) {
-			locations_initialize_map(key, locations[key]);
-		}
-	}
+    for (var key in locations) {
+        if (locations.hasOwnProperty(key)) {
+            locations_initialize_map(key, locations[key]);
+        }
+    }
 
 }
 
 function locations_get_canvas_id(id) {
-	return 'locations-map-canvas-' + id;
+    return 'locations-map-canvas-' + id;
 }
 
 function locations_initialize_map(id, options) {
 
-	var mapOptions = {
-		center: { lat: -34.397, lng: 150.644},	
-		zoom: parseInt(options.zoom),
-		disableDefaultUI: true
-	};
+    var mapOptions = {
+        center: { lat: -34.397, lng: 150.644},    
+        zoom: parseInt(options.zoom),
+        disableDefaultUI: true
+    };
 
-	var canvasId = locations_get_canvas_id(id);
-        var map = new google.maps.Map(document.getElementById(canvasId), mapOptions);
-	var bounds = new google.maps.LatLngBounds();
-	var coordinates = [];
+    var canvasId = locations_get_canvas_id(id);
+    var map = new google.maps.Map(document.getElementById(canvasId), mapOptions);
+    var bounds = new google.maps.LatLngBounds();
+    var coordinates = [];
 
-	for (var i = 0; i < options.pins.length; i++) {
+    for (var i = 0; i < options.pins.length; i++) {
 
-		var pin = options.pins[i];
-		var latlng = new google.maps.LatLng(pin.lat, pin.lng);
+        var pin = options.pins[i];
+        var latlng = new google.maps.LatLng(pin.lat, pin.lng);
 
-		bounds.extend(latlng);
+        bounds.extend(latlng);
 
-		var marker = new google.maps.Marker({
-			position: latlng,
-			map: map,
-			title: pin.name
-		});
+        var marker = new google.maps.Marker({
+            position: latlng,
+            map: map,
+            title: pin.name
+        });
 
-		coordinates.push(latlng);
+        coordinates.push(latlng);
 
-	}
+    }
 
-	var path = new google.maps.Polyline({
-		path: coordinates,
-		geodesic: true,
-		strokeColor: '#ff0000',
-		strokeOpacity: 1.0,
-		strokeWeight: 2
-	});
+    var path = new google.maps.Polyline({
+        path: coordinates,
+        geodesic: true,
+        strokeColor: '#ff0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
 
-	path.setMap(map);
-	map.fitBounds(bounds);
+    path.setMap(map);
+    map.fitBounds(bounds);
 
-	locations_map_instances[id] = map;
+    locations_map_instances[id] = map;
 }
 
 var locations = [];
@@ -62,8 +62,8 @@ var locations_map_instances = {};
 google.maps.event.addDomListener(window, 'load', locations_initialize);
 
 function setLocation(id, index) {
-	var map = locations_map_instances[id];
-	var location = locations[id];
-	var pin = location.pins[index];
-	map.setCenter(new google.maps.LatLng(pin.lat, pin.lng));
+    var map = locations_map_instances[id];
+    var location = locations[id];
+    var pin = location.pins[index];
+    map.setCenter(new google.maps.LatLng(pin.lat, pin.lng));
 }
